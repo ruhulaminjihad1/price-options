4b766ee7809520e280646843f836bc91e4836e1c
Ruhul Amin Zihad <ruhulaminjihad1234@gmail.com>
2023-09-18 10:25:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+ 
+ 
+import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+ 
+
+  return (
+     <> <Navbar></Navbar>
+     <h1 className='text-7xl bg-rose-200'>Vite + React</h1>
+     </>  
+      
+    
+  )
+}
+
+export default App
